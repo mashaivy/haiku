69e25b6f5771441dfb399ca13d5a690c8fcdb07b
devin cooley and masha ivy <masha@>
2019-09-05 14:37:21 -0700

    Add form field to html, write form field to html, write Poem class and checkLines prototype

---
+++ spec/haiku-spec.js
@@ -0,0 +1,8 @@
+import { Poem } from './../src/haiku.js';
+
+describe('Lines', function() {
+
+  it('should test whether the poem has three lines', function(){
+    expect(true).toEqual(false);
+  })
+});
