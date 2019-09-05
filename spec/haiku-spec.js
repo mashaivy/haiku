4a819d078aecc13c49e5461369b5e84c9b2ebd45
devin cooley and masha ivy <masha@>
2019-09-05 16:47:07 -0700

    Add split poem function

--- spec/haiku-spec.js
+++ spec/haiku-spec.js
@@ -3,6 +3,11 @@ import { Poem } from './../src/haiku.js';
 describe('Lines', function() {
 
   it('should test whether the poem has three lines', function(){
-    expect(true).toEqual(false);
+    let userInput = "This is my line 1 \r\n this is my line 2 \r\n this is my line 3 ";
+    let newPoem = new Poem(userInput);
+    console.log(newPoem.entireP)
+    console.log(newPoem.entireP.split(/\r\n|\r|\n/));
+    console.log(newPoem.checkLines());
+    expect(newPoem.checkLines()).toEqual(true);
   })
 });
